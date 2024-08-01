7a1591db842eaaafe1fa7c8cdf34908e3ffa59f2
Fernando Falcon <fer.falcon.dg@gmail.com>
2024-08-01 17:37:09 -0300

    Contentless

--- behavior.js
+++ behavior.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(event){
-    
+    var thumbnailElement = document.getElementById("smart_thumbnail");
+    thumbnailElement.addEventListener("click", function() {
+        if (thumbnailElement.className == "") {
+            thumbnailElement.classList.add("small");
+        } else {
+            thumbnailElement.classList.remove("small");
+        }
+    });
 });
